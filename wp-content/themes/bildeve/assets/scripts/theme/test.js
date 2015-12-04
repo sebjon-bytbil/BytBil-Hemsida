6f6201161d45bd2876a712562b65a5d746548eea
sebjon-bytbil <sebastian.jonsson@bytbil.com>
2015-12-04 05:50:29 +0100

    Theme Additions

---
+++ wp-content/themes/bildeve/assets/scripts/theme/test.js
@@ -0,0 +1,8 @@
+;(function($, window){
+    $('.selectpicker').selectpicker();
+
+    var screen_width = window.innerWidth;
+    var screen_height = window.innerHeight;
+
+    $('.bb-slideshow').flexslider();
+})(jQuery, window);
